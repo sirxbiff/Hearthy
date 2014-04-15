fe514e5c7adf338200e7657274bea1a8c3730089
Matthijs Piek <matthijs@sandcat.nl>
2014-04-15 09:40:31 +0200

    Do not display missions set for github card source

--- github_card_source.rb
+++ github_card_source.rb
@@ -4,6 +4,6 @@ require 'json'
 class GithubCardSource
   def cards
     gianthash = JSON.parse(open('all-cards.json').read)
-    gianthash["cards"].map{ |c| GithubHashCard.new(c) }
+    gianthash["cards"].select{ |c| c["set"] != "missions" }.map{ |c| GithubHashCard.new(c) }
   end
 end
